5c7896fabf30f046d05f4fe3a724f29eef0c6770
Remko Boschker <remkoboschker@Remkos-MacBook-Pro.local>
2017-02-11 15:07:59 +0100

    adds comments to existing code, adds argument parser, changes training data filename

--- Assignments/LFDassignment1.py
+++ Assignments/LFDassignment1.py
@@ -1,10 +1,31 @@
+import argparse
+
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
-
 from sklearn.metrics import accuracy_score
 
-# COMMENT THIS
+parser = argparse.ArgumentParser(
+    prog='naive bayes classifier',
+    description='classifies reviews by sentiment of topic'
+)
+parser.add_argument(
+    '--use_sentiment',
+    default=False,
+    type=bool,
+    dest='use_sentiment',
+    help='boolean to select the use of sentiment labels',
+    metavar='use sentiment',
+    choices=['true', 'false']
+)
+
+args = parser.parse_args()
+
+
+# This function takes a filename and a boolean that selects
+# whether the sentiment or topic labels are used.
+# It returns two array's one containing the review texts
+# and another containing the labels.
 def read_corpus(corpus_file, use_sentiment):
     documents = []
     labels = []
@@ -14,7 +35,7 @@ def read_corpus(corpus_file, use_sentiment):
 
             documents.append(tokens[3:])
 
-            if use_sentiment:
+            if args.use_sentiment:
                 # 2-class problem: positive vs negative
                 labels.append( tokens[1] )
             else:
@@ -22,18 +43,20 @@ def read_corpus(corpus_file, use_sentiment):
                 labels.append( tokens[0] )
 
     return documents, labels
-    
+
 # a dummy function that just returns its input
 def identity(x):
     return x
 
-# COMMENT THIS
-X, Y = read_corpus('training.txt', use_sentiment=True)
-split_point = int(0.75*len(X))
-Xtrain = X[:split_point]
-Ytrain = Y[:split_point]
-Xtest = X[split_point:]
-Ytest = Y[split_point:]
+# Calls read corpus with trainset.txt as a filename and using the sentiment labels
+# assigning three quarters of the documents and labels as a training set and one quarter as
+# a test set
+documents, labels = read_corpus('trainset.txt', use_sentiment=True)
+split_point = int(0.75*len(documents))
+documentsTrain = documents[:split_point]
+labelsTrain = labels[:split_point]
+documentsTest = documents[split_point:]
+labelsTest = labels[split_point:]
 
 # let's use the TF-IDF vectorizer
 tfidf = True
@@ -47,17 +70,20 @@ else:
     vec = CountVectorizer(preprocessor = identity,
                           tokenizer = identity)
 
-# combine the vectorizer with a Naive Bayes classifier
+# Combines the vectorizer with a Naive Bayes classifier
 classifier = Pipeline( [('vec', vec),
                         ('cls', MultinomialNB())] )
 
 
-# COMMENT THIS
-classifier.fit(Xtrain, Ytrain)
+# Fits a naive bayes classifier according to the training vectors that come from
+# the vectorization of the documents to the target values in the label training set.
+classifier.fit(documentsTrain, labelsTrain)
 
-# COMMENT THIS  
-Yguess = classifier.predict(Xtest)
+# Performs classification on the vecorization of the documents in the test set.
+labelsGuess = classifier.predict(documentsTest)
 
-# COMMENT THIS
-print(accuracy_score(Ytest, Yguess))
+# Prints the accuracy score of the predicted labels compares to the labels in the test set.
+# The accuracy score is the fraction of correct predictions with respect to all predictions;
+# in multilabel classification it prints the subclass accuracy.
+print(accuracy_score(labelsTest, labelsGuess))
 
