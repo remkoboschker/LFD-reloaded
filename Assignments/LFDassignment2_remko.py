fdc9dbc1c41bd61cfe05e20f22ff1b96e7b085b3
Remko Boschker <remko@informatietuin.nl>
2017-02-26 22:11:35 +0100

    eval representations

--- Assignments/LFDassignment2_remko.py
+++ Assignments/LFDassignment2_remko.py
@@ -1,13 +1,15 @@
 import argparse
+import time
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, precision_recall_fscore_support, make_scorer, f1_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk import pos_tag
+from sklearn.model_selection import cross_val_score, cross_val_predict
 
 stemmer = PorterStemmer()
 lemmatiser = WordNetLemmatizer()
@@ -56,23 +58,12 @@ def read_corpus(corpus_file, use_sentiment):
 def identity(x):
     return x
 
-# Calls read corpus with trainset.txt as a filename and using the sentiment labels
-# assigning three quarters of the documents and labels as a training set and one quarter as
-# a test set
-documents, labels = read_corpus('trainset.txt', args.use_sentiment)
-split_point = int(0.75*len(documents))
-documentsTrain = documents[:split_point]
-labelsTrain = labels[:split_point]
-documentsTest = documents[split_point:]
-labelsTest = labels[split_point:]
-
 # let's use the TF-IDF vectorizer
 tfidf = True
 
 # the wordnet lemmatizer uses the wordnet tags NOUN, VERB, ADV, ADJ
 # the pos tagger outputs Penn Treebank tags, so we convert
-def mapPosToWordNetLem(par):
-    (token, tag) = par
+def mapPosToWordNetLem(token, tag):
     if tag == 'VB' or tag == 'VBD' or tag == 'VBN' or tag == 'VBP' or tag == 'VBZ':
         return lemmatiser.lemmatize(token, pos='v')
     if tag == 'RB' or tag == 'RBR' or tag == 'RBS':
@@ -83,7 +74,8 @@ def mapPosToWordNetLem(par):
 
 
 def lemmatise(doc):
-    return map(mapPosToWordNetLem, pos_tag(doc))
+    # pos_tag returns a (token, tag) tuple, that needs to be unpacked
+    return map(lambda x: mapPosToWordNetLem(*x), pos_tag(doc))
 
 def stem(doc):
     return map(lambda x: stemmer.stem(x), doc)
@@ -93,28 +85,74 @@ def stem(doc):
 # we use a dummy function as tokenizer and preprocessor,
 # since the texts are already preprocessed and tokenized.
 if tfidf:
-    vec = TfidfVectorizer(preprocessor = lemmatise,
-                          tokenizer = identity)
+    vec = TfidfVectorizer(preprocessor = stem,
+                          tokenizer = identity,
+                          stop_words = 'english')
 else:
     vec = CountVectorizer(preprocessor = identity,
-                          tokenizer = identity)
+                          tokenizer = identity,
+                          stop_words = 'english')
 
 # Combines the vectorizer with a Naive Bayes classifier
 classifier = Pipeline( [('vec', vec),
-                        ('cls', MultinomialNB())] )
+                        ('cls', KNeighborsClassifier())] )
+
+# Calls read corpus with trainset.txt as a filename and using the sentiment labels
+# assigning three quarters of the documents and labels as a training set and one quarter as
+# a test set
+documents, labels = read_corpus('trainset.txt', args.use_sentiment)
+# split_point = int(0.75*len(documents))
+# documentsTrain = documents[:split_point]
+# labelsTrain = labels[:split_point]
+# documentsTest = documents[split_point:]
+# labelsTest = labels[split_point:]
 
 
-# Fits a naive bayes classifier according to the training vectors that come from
-# the vectorization of the documents to the target values in the label training set.
-classifier.fit(documentsTrain, labelsTrain)
 
-# Performs classification on the vecorization of the documents in the test set.
-labelsGuess = classifier.predict(documentsTest)
+# # Performs classification on the vecorization of the documents in the test set.
+# labelsGuess = classifier.predict(documentsTest)
 
-# Prints a report containing the f-score, precision, recall and number of supporting instances
-print(classification_report(labelsTest, labelsGuess))
-# Prints the confusion matrix
-print(confusion_matrix(labelsTest, labelsGuess))
+# def printResults():
+#     # Prints a report containing the f-score, precision, recall and number of supporting instances
+#     print(classification_report(labelsTest, labelsGuess))
+#     # Prints the confusion matrix
+#     print(confusion_matrix(labelsTest, labelsGuess))
+#
+#     print(classifier.predict_proba(documentsTest[:10]))
+#
+# def run(XTrain, XTest, yTrain, yTest):
+#     t0 = time.time()
+#     # Fits a naive bayes classifier according to the training vectors that come from
+#     # the vectorization of the documents to the target values in the label training set.
+#     classifier.fit(XTrain, yTrain)
+#     trained = time.time()
+#     predictions = classifier.predict(XTest)
+#     guessed = time.time()
+#     return precision_recall_fscore_support(yTest, predictions)
+#
+# def cross_validation(X,y,parts):
+#     scores = []
+#     for index
 
-print(classifier.predict_proba(documentsTest[:10]))
+# # do not uses this because hard to insert times and would like
+# # precision and recall out as wel
+# scores = cross_val_score(
+#     estimator=classifier,
+#     X=documents,
+#     y=labels,
+#     scoring=make_scorer(f1_score, average='macro'),
+#     cv=4,
+#     verbose=3
+# )
+
+predictions = cross_val_predict(
+    estimator=classifier,
+    X=documents,
+    y=labels,
+    cv=4,
+    verbose=3
+)
+
+print(classification_report(labels, predictions, digits=4))
+
 
